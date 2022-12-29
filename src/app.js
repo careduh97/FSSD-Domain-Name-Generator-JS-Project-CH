7234a4359830fe3e9077a26f7bca40f455d591da
Carlos Herrera <c.herrerav97@gmail.com>
2022-12-29 04:56:32 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net"];
+
+  function domainGenerator(array1, array2, array3, array4) {
+    let domainArray = [];
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        for (let x = 0; x < array3.length; x++) {
+          for (let y = 0; y < array4.length; y++) {
+            domainArray.push(
+              `${array1[i]}${array2[j]}${array3[x]}${array4[y]}`
+            );
+          }
+        }
+      }
+    }
+    for (let k = 0; k < domainArray.length; k++) {
+      console.log(domainArray[k]);
+    }
+  }
+  domainGenerator(pronoun, adj, noun, ext);
 };
